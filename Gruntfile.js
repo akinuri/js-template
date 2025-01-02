f2286fa1b7dd4300e4b08438ec7b78c69d17522c
Noreh AD <akinoreh@gmail.com>
2025-01-02 19:39:29 +0300

    grunt: watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
@@ -20,7 +21,17 @@ module.exports = function (grunt) {
                 dest: "dist/template.js",
             },
         },
+
+        watch: {
+            scripts: {
+                files: ["src/**/*.js"],
+                tasks: ["concat"],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
     });
 
-    grunt.registerTask("default", ["concat"]);
+    grunt.registerTask("default", ["concat", "watch"]);
 };
