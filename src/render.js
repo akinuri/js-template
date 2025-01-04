1afc37b8f2824a2f2cd8026db9b0cb6d1d349b55
Noreh AD <akinoreh@gmail.com>
2025-01-04 14:58:48 +0300

    src/render.js: update docblocks

--- src/render.js
+++ src/render.js
@@ -13,7 +13,7 @@ class OutputBuffer {
 /**
  * Renders the expressions in the template string.
  * @param {string} templateString Raw HTML string with expressions.
- * @param {object} [data={}] Key-value pairs to be used in the expressions.
+ * @param {object} [data={}] Key-value pairs to be used in the expressions. The keys are the variable names and the values are their corresponding values.
  * @returns {*} The processed HTML string with expressions replaced.
  */
 function replaceTemplateExpressions(templateString, data = {}) {
@@ -29,7 +29,7 @@ function replaceTemplateExpressions(templateString, data = {}) {
             text += ob;
             return text;
         } catch (error) {
-            console.warn(`Error evaluating expression: ${expression}`, error);
+            console.warn(`Error evaluating the expression: ${expression}`, error);
             return "";
             return match;
         }
@@ -37,9 +37,10 @@ function replaceTemplateExpressions(templateString, data = {}) {
 }
 
 /**
- * @param {string} expression A JS expression.
- * @param {object} context Key-value pairs.
- * @returns {*} The expression result.
+ * Evaluates the expression with the given context.
+ * @param {string} expression The expression to evaluate.
+ * @param {object} context The context object containing the variables used in the expression.
+ * @returns {*} The result of the expression.
  */
 function evaluateExpression(expression, context) {
     return new Function(...Object.keys(context), `return ${expression};`)(...Object.values(context));
